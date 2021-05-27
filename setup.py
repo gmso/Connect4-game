0d8f0509f6e19cf9796f13fa8a89b769e57bb4c0
gmso <german.mene@gmail.com>
2021-05-27 20:51:54 +0200

    changes for automated tests (tox) and dependency freeze (requirements.txt

--- setup.py
+++ setup.py
@@ -1,3 +1,23 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name="Connect4", packages=find_packages())
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="connect-4-cli",
+    version="0.0.1",
+    author="Germán Mené Santa Olaya",
+    author_email="german.mene@gmail.com",
+    description="A CLI implementation of the classic connect 4 game",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gmso/Connect4-game",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Environment :: Console",
+        "Topic :: Games/Entertainment :: Board Games"
+    ],
+)
