dfd8f0345f16a80504b601eeb773646cf2237dbe
gmso <german.mene@gmail.com>
2021-05-28 13:28:19 +0200

    patch pynput.keyboard on test

--- test/utility_InputKeyStrokes.py
+++ test/utility_InputKeyStrokes.py
@@ -1,8 +1,15 @@
-from src.PlayerInput import PlayerInput
-
+from unittest.mock import patch
 
 def simulate_key_press(key):
+
+    patcher = patch('pynput.keyboard', 0)
+    patcher.start()
+
+    from src.PlayerInput import PlayerInput
+
     input = PlayerInput()
     key_not_detected_at_first = input.key_press_detected
     key_detected_after, detected_key = input.wait_for_user_input(key)
+
+    patcher.stop()
     return (key_not_detected_at_first, key_detected_after, detected_key)
