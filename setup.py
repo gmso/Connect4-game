94deec368903a5bd14d8dc162a01493834e658c3
gmso <german.mene@gmail.com>
2021-05-28 19:14:05 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -12,12 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/gmso/connect-4-cli",
-    packages = setuptools.find_packages(
-        where = 'src',
-        include = ['App*',],
-        exclude = ['test',]
-    ),
-    package_dir = {"":"src"},
+    packages = setuptools.find_packages(include = ['src*',]),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
